279aa263f23343e943dcd74fea6f8ea289fcae80
Piotrull0 <113345346+Piotrull0@users.noreply.github.com>
2025-03-04 18:25:17 +0100

    Add files via upload

---
+++ Cwiczenia1/zad2.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Zadanie 2</title>
+</head>
+<body>
+<h1>Special characters</h1>
+<h3>Quadric equation</h3>
+<p>-&#189x<sup>2</sup>+ 4x + 10</p>
+<p>&#8667 &#8710 = 16 + 20</p>
+<p>&#8667 &#8730 &#8710 = &#8730 26</p>
+<p>x<sub>1</sub> = 4 + &#8730 26 &nbsp x<sub>2</sub> = 4 - &#8730 26</p>
+Author: Friedrich B&#246;mn, 2022.
+</body>
+</html>
