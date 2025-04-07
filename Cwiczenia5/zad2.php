0b73812353d82f62f12ddf6b2da226e86933deb3
Piotrull0 <113345346+Piotrull0@users.noreply.github.com>
2025-04-08 00:34:17 +0200

    Add files via upload

---
+++ Cwiczenia5/zad2.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Zadanie 2</title>
+    <link rel="stylesheet" href="zad2.css">
+</head>
+    <div class="header">
+        <h1>LOGO</h1>
+        <nav class="nav">
+            <ul class="nav-list">
+                <li><a href="">Strona główna</a></li>
+                <li><a href="">O nas</a></li>
+                <li><a href="">Kontakt</a></li>
+            </ul>
+        </nav>
+    </div>
+</body>
+</html>
---
+++ Cwiczenia5/zad4.php
@@ -0,0 +1,43 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Zadanie 4</title>
+    <link rel="stylesheet" href="zad4.css">
+</head>
+<body>
+    <div class="main">
+        <nav class="nav">
+            <ul>
+                <li><a href="">Strona główna</a></li>
+                <li><a href="">O nas</a></li>
+                <li><a href="">Kontakt</a></li>
+            </ul>
+        </nav>
+        <div class="content">
+            <div class="title">
+                <h2>Witaj na naszej skomplikowanej stronie!</h2>
+            </div>
+            <div class="cont1">
+                <h3>Funkcja 1</h3>
+                <p>Opis funkcji 1...</p>
+            </div>
+            <div class="cont2">
+                <h3>Funkcja 2</h3>
+                <p>Opis funkcji 2...</p>
+            </div>
+            <div class="cont3">
+                <h3>Funkcja 3</h3>
+                <p>Opis funkcji 3...</p>
+            </div>
+            <div class="cont-right">
+                <h3>Dodatkowe informacje</h3>
+                <p>...</p>
+            </div>
+        </div>
+        <div class="footer">
+            <p>&#169 2023 Skomplikowana strona z flexboxem. Wszelkie prawa zastrzeżone.</p>
+        </div>
+    </div>
+</body>
+</html>
